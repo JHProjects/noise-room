f5c6fd6c7bb7f559b798488ca283c8a1d1ff8538
Jan Holeček <jan.holecek@post.cz>
2025-05-26 02:15:54 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const http = require("http")
+const fs = require("fs")
 const cors = require("cors")
 const path = require("path")
 const { Server } = require("socket.io")
